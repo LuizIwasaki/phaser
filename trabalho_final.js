f37c0b658926ceb88c5e1a0ff341a98b9f2d7d93
LuizIwasaki <70579914+LuizIwasaki@users.noreply.github.com>
2020-10-07 00:07:58 -0300

    Add files via upload

---
+++ trabalho_final.js
@@ -0,0 +1,73 @@
+
+
+function scroll(id) {
+    
+    $('html, body').animate({
+        scrollTop: $(id).offset().top
+    }, 720, function () {
+        window.location.hash = id;
+    });
+
+}
+
+$(function () {
+
+    console.log("Apresentando seções");
+
+    $("#Secao1").on("click", function () {
+        console.log("IR PARA A HOME");
+        scroll("#secao1");
+    });
+
+    $("#Secao2").on("click", function () {
+        console.log("funcao ir para secao 1 acionada..");
+        scroll("#secao2");
+    });
+
+    $("#Secao3").on("click", function () {
+        console.log("funcao ir para secao 2 acionada..");
+        scroll("#secao3");
+    });
+    $("#Secao4").on("click", function () {
+        console.log("funcao ir para secao 2 acionada..");
+        scroll("#secao4");
+    });
+
+    
+
+
+});
+
+function altoContraste() {
+    console.log("funcao gerenciar alto contraste");
+    $("body").toggleClass("cor-alto-contraste");
+
+}
+$(document).ready(function(){ // jquery
+  
+
+    console.log("Entrou no window onload..funcionou o JS");
+   
+    $("#firstContraste").on("click", altoContraste);   
+
+
+});
+
+
+function segundo() {
+    console.log("funcao gerenciar alto contraste");
+    $("body").toggleClass("secondcolor");
+
+}
+$(document).ready(function()
+{ 
+  
+
+    console.log("Entrou no window onload..funcionou o JS");
+   
+    $("#secondContraste").on("click", segundo);   
+
+});
+
+
+
