eb4f62f5bb731f495eeac6a59a25e06addf36c80
LuizIwasaki <70579914+LuizIwasaki@users.noreply.github.com>
2020-10-09 00:09:19 -0300

    Add files via upload

--- trabalho_final.js
+++ trabalho_final.js
@@ -33,8 +33,20 @@ $(function () {
         scroll("#secao4");
     });
     $("#Secao5").on("click", function () {
-        console.log("ir para Formularios..");
+        console.log("ir para as referencias..");
         scroll("#secao5");
+    });
+    $("#Secao6").on("click", function () {
+        console.log("ir para as referencias..");
+        scroll("#secao6");
+    });
+    $("#Secao7").on("click", function () {
+        console.log("funcao ir para secao 7 acionada..");
+        scroll("#secao7");
+    });
+    $("#Secao8").on("click", function () {
+        console.log("ir para Formularios..");
+        scroll("#secao8");
     });
     $("#Secao9").on("click", function () {
         console.log("ir para as referencias..");
@@ -43,53 +55,39 @@ $(function () {
 
     
 
-
 });
 
 function altoContraste() {
     console.log("funcao gerenciar alto contraste");
     $("body").toggleClass("first-color");
     
-
 }
-$(document).ready(function(){ // jquery
-  
 
-    console.log("Entrou no window onload..funcionou o JS");
-   
-    $("#firstContraste").on("click", altoContraste);   
-
-
-});
-
-
-function segundo() {
+function extraContraste() {
     console.log("funcao gerenciar alto contraste");
     $("body").toggleClass("secondcolor");
 
 }
+
+function extraContraste() {
+    console.log("funcao gerenciar alto contraste");
+    $("body").toggleClass("secondcolor");
+
+}
+
+
+
+
 $(document).ready(function()
 { 
   
-
-    console.log("Cor extra em funcionamento");
+    console.log("Entrou no window onload..funcionou o JS");
    
-    $("#secondContraste").on("click", segundo);   
+    $("#btn-firstContraste").on("click", altoContraste);   
+
+    $("#btn-secondContraste").on("click", extraContraste); 
+
+    $("#btn-threecolor").on("click", ContrasteReverso);
 
 });
 
-function ContrasteReverso() {
-    console.log("funcao gerenciar alto contraste");
-    $("firstcolor").toggleClass("threecolor");
-    
-
-}
-$(document).ready(function(){ // jquery
-  
-
-    console.log("Contraste Normal em funcionamento");
-   
-    $("#normalContraste").on("click", ContrasteReverso);
-
-
-});
